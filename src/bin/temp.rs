860a92f8029db5a4e68c46ea9f35b4768ba3a25f
K V Varun Krishnan <varunkkv.mec@gmail.com>
2024-01-19 16:41:38 +0530

    temp Request and Response

--- src/bin/temp.rs
+++ src/bin/temp.rs
@@ -1,8 +1,30 @@
-use std::net::TcpListener;
-fn main(){
-    let listner = TcpListener::bind("127.0.0.1:7878").unwrap();
-    for stream in listner.incoming() {
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("COnnection established");
+
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let status_line = "HTTP/1.1 200 OK ";
+    let content = "youtube.com/watch?v=dqw4w9wgxcq";
+    let length = content.len();
+    let response = format!("{status_line} \r\n Content-length:{length} \r\n {content}");
+    stream.write_all(response.as_bytes()).unwrap()
+
 }
