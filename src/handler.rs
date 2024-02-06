448b56c3a68436775839a77a6abdb842c1b9bb12
K V Varun Krishnan <varunkkv.mec@gmail.com>
2024-02-06 08:55:18 +0530

    api handlers

---
+++ src/handler.rs
@@ -0,0 +1,14 @@
+pub mod handler{
+    use axum::handler::Handler;
+    use axum::{routing::get, Json};
+    pub async fn handle_api() -> Json<serde_json::Value> { // Add async keyword and return type
+        const message:&str = "Hello world";
+    
+        let json_resp = serde_json::json!({
+            "status": "OK",
+            "Message":message,
+        });
+    
+        Json(json_resp)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use std::net::TcpListener;
 use axum::{routing::get, Json, Router};
-use axum::handler::Handler; // Import the Handler trait
+ // Import the Handler trait
 use tokio::net::TcpListener as TokioTcpListener; // Remove duplicate import
+
+mod actions;
+mod handler;
 
 #[tokio::main]
 
 async fn main(){
-    let app = Router::new().route("/api/hello", get(handle_api));
+    let app = Router::new().route("/api/hello", get(handler::handle_api));
 
     let listener = TokioTcpListener::bind("127.0.0.1:7878").await.unwrap(); // Use tokio::net::TcpListener::bind
 
@@ -15,13 +18,3 @@ async fn main(){
     axum::serve(listener, app.into_make_service()).await.unwrap();
 }
 
-pub async fn handle_api() -> Json<serde_json::Value> { // Add async keyword and return type
-    const message:&str = "Hello world";
-
-    let json_resp = serde_json::json!({
-        "status": "OK",
-        "Message":message,
-    });
-
-    Json(json_resp)
-}
