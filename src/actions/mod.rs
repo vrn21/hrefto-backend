cf58a0073c1c4c0c13ba7d7207c2aa1383b3e205
K V Varun Krishnan <varunkkv.mec@gmail.com>
2024-02-07 23:07:06 +0530

    resolved some errors

--- src/actions/mod.rs
+++ src/actions/mod.rs
@@ -1,43 +1,44 @@
 pub mod db{
-    use postgresql::{Client,Error,NoTls};
-    use postgresql::types::Type;
+    use postgres::{Client, Error, NoTls};
 
- struct Link{
-    long_url: &str,
-    short_url:&str
- }
+    struct Link {
+        long_url: &str,
+        short_url: &str,
+    }
 
- pub fn SELECT(client:&mut Client, s_url: &str) -> link{
-    let db_query = client.query("SELECT longurl FROM hrefto WHERE shorturl = $1",s_url);
-    
-    let link = Link {
-        long_url: db_query.get(),
-        short_url: s_url
-    };
+    pub fn select(client: &mut Client, s_url: &str) -> Link {
+        let db_query = client.query("SELECT longurl FROM hrefto WHERE shorturl = $1", &[&s_url]).unwrap();
 
-    link
- }
+        let link = Link {
+            long_url: db_query.get(0).get(0),
+            short_url: s_url,
+        };
 
- pub fn INSERT(client:&mut Client, l_url: &str, s_url: &str) -> link{
+        link
+    }
 
-    if CHECK(client, l_url, s_url) == 0{
-        let link = Link{
+ pub fn insert(client:&mut Client, l_url: &str, s_url: &str) -> Link{
+    let link;
+    if check(client, l_url, s_url) == 0{
+        link = Link{
             long_url: l_url,
             short_url: s_url
         };
 
         client.execute("INSERT INTO hrefto (longurl,shorturl) VALUES ($1, $2)", &link.long_url,&link.short_url);
     }else{
-        let link = SELECT(client, s_url);
+        link = select(client, s_url);
     }
     link
  }
 
- pub fn CHECK(client:&mut Client, l_url: &str, s_url: &str) -> i8{
-    let db_link = SELECT(client, s_url);
-    if db_link == l_url {
+pub fn check(client:&mut Client, l_url: &str, s_url: &str) -> i8{
+    let db_link = select(client, s_url);
+    if db_link.long_url == l_url {
         1
-    } 0
+    } else {
+        0
+    }
 }
 
 
